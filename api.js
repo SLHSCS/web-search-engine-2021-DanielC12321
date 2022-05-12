0d38b99572cf2024f23478ee478322e29eeb5740
DanielC12321 <73292458+DanielC12321@users.noreply.github.com>
2022-05-12 07:37:13 -0500

    Add files via upload

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
-const port = 3000
+const port = 3001
 const score = require('./scoreweb.js');
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -72,6 +72,9 @@ app.listen(port, () => {
           
         
           }
+          respo.results.splice(count);
+          respo.numres = respo.results.length;
+          
           res.status(200).send(respo); 
     });
 
@@ -83,6 +86,81 @@ app.listen(port, () => {
     }
  
     
+});
+app.get('/:key/:s', (req, res) => {
+  // Reading isbn from the URL
+  
+  const key = req.params.key;
+  const s = req.params.s;
+  const count = req.params.count;
+  var respo = {
+    numres: 0,
+    keyword: s,
+    results: []
+};
+  if(key!=123)
+  {
+      res.status(404).send('key not found');
+  }
+  else{
+    fs.readFile('scrapedata.json', function (err, data) {
+      var json = JSON.parse(data);
+      
+      for (let i of json["table"]) {
+        
+        
+        try{
+        if(i["content"].includes(s))
+        {
+         var scorexD =score.scoreHTML(i["content"], s);
+           if(scorexD>0 )
+           {
+            respo["numres"] = respo["numres"]+1;
+            try
+            {
+              var title2 = i["content"].match(/\<[tT]itle>([^()]*)\<\/[tT]itle>/)[1];
+            }
+            catch(error)
+            {
+              var title2 = "none";
+            }
+            let link = {
+              
+              title: title2,
+              url: i["url"],
+              score: scorexD
+          };
+          console.log(link);
+            respo["results"].push(link);
+            console.log("hi");
+            console.log(respo);
+           }
+           
+         
+         
+       
+         
+        }
+      }
+      catch(error)
+      {
+
+      }
+        
+      
+        }
+        
+        res.status(200).send(respo); 
+  });
+
+
+
+
+
+
+  }
+
+  
 });
 
 
